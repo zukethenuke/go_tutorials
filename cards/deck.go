a95a7958e7ba883a05885d60024b5cfb52770eaa
Stephen Zukowski <stephenzukowski@Stephens-MacBook-Pro.local>
2020-08-09 14:46:36 -0500

    Add deck type

---
+++ cards/deck.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Create and type of 'deck'
+// which is a slice of strings
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
